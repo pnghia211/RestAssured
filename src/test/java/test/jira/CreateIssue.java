515eaa83665dc844c244728a8f12583c35b0b9f8
pnghia211 <pnghia211@gmail.com>
2024-03-06 13:27:29 +0700

    lesson-10

--- src/test/java/test/jira/CreateIssue.java
+++ src/test/java/test/jira/CreateIssue.java
@@ -3,11 +3,15 @@ package test.jira;
 import builder.IssueContentBuilder;
 import builder.JSONBuilder;
 import data.Jira.IssueInfo;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.commons.lang3.RandomStringUtils;
 import utils.ProjectInfo;
 import utils.RequestCapability;
 import utils.AuthTokenHandler;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -32,11 +36,38 @@ public class CreateIssue implements RequestCapability {
         // Define body data
         ProjectInfo projectInfo = new ProjectInfo(baseUri, projectKey);
         String taskTypeId = projectInfo.getIssueTypeId("task");
-        String summary = "This is my summary from API";
-        String projectInfoStr = IssueContentBuilder.build(summary, projectKey, taskTypeId);
+
+        int length = 20;
+        boolean hasText = true;
+        boolean hasNum = true;
+        String randomSummary = RandomStringUtils.random(length,hasText,hasNum);
+
+        IssueContentBuilder issueContentBuilder = new IssueContentBuilder();
+        String projectInfoStr = issueContentBuilder.build(randomSummary, projectKey, taskTypeId);
 
         // Send request
         Response response = request.body(projectInfoStr).post(path);
         response.prettyPrint();
+
+        // Get request
+        Map<String, String> responseBody = JsonPath.from(response.asString()).get();
+        String getIssuePath = "/rest/api/3/issue/" + responseBody.get("key");
+
+        response = request.get(getIssuePath);
+
+        IssueInfo issueInfo = issueContentBuilder.getIssueInfo();
+        String expectedSummary = issueInfo.getFields().getSummary();
+        String expectedStatus = "To Do";
+
+
+        JsonPath jsonPath = new JsonPath(response.asString());
+        String actualSummary = jsonPath.getString("fields.summary");
+        String actualStatus = jsonPath.getString("fields.status.statusCategory.name");
+
+        System.out.println(expectedSummary);
+        System.out.println(actualSummary);
+
+        System.out.println(expectedStatus);
+        System.out.println(actualStatus);
     }
 }
